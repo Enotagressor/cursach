c173c150bf42f06464c591ccecb61e8b0d56ee66
Sergey <enotagressor@gmail.com>
2022-08-29 18:22:33 +0700

    1

---
+++ src/test/java/pro/sky/cursach/CursachApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.cursach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
